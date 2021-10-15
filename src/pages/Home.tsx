f87f357c15e47fac1f4fde554462a950c222a1a9
Luis Aniceto <luisg.aniceto@hotmail.com>
2021-10-15 17:49:25 -0300

    add new task

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,23 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+interface NewTask {
+  id : number;
+  title : string;
+  done : boolean;
+}
+
 export function Home() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<NewTask[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, data] );
   }
 
   function handleToggleTaskDone(id: number) {
